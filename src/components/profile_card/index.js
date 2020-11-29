e7885a970ab55148cb59b6ed781c5cc1ea22209d
Klaus <ksf.klausfreire@gmail.com>
2020-11-29 14:32:56 -0300

    complete personal card

--- src/components/profile_card/index.js
+++ src/components/profile_card/index.js
@@ -1,7 +1,7 @@
 import { Card , Avatar, CardHeader, CardContent, CardMedia, Grow} from '@material-ui/core';
 import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap';
-import { MdPerson } from 'react-icons/md';
+import { Col, Container } from 'react-bootstrap';
+import { FaUser, FaEnvelope, FaPhone, FaLinkedin , FaGithub} from 'react-icons/fa';
 import { useStyles } from '../../theme/global_styles';
 
 export default function ProfileCard(){
@@ -27,15 +27,28 @@ export default function ProfileCard(){
                     />
                 </CardMedia>
                 <CardContent className={classes.cardContent}>
-                    <Container>
-                        <Row>
-                            <Col>
-                                <MdPerson size="2rem"/>
-                            </Col>
-                            <Col>
-                                <p> rwerwkljkl difadfsdaf fdsafdafa fdsafdaf fdsafdsafsa</p>
-                            </Col>
-                        </Row>
+                    <Container style={{textAlign: "left"}}>
+                        <Col>
+                            <FaUser  style={{marginRight: "0.5rem"}}/>
+                            About
+                            <p>Desenvolvedor FullStack. Atuante em tecnologias Mobile e Web como Flutter, Node e Php.</p>
+                        </Col>
+                        <Col>
+                            <FaEnvelope style={{marginRight: "0.5rem"}}/>
+                            ksf.klausfreire@gmail.com
+                        </Col>
+                        <Col>
+                            <FaPhone style={{marginRight: "0.5rem"}}/>
+                            (61) 98271-5186
+                        </Col>
+                        <Col>
+                            <FaLinkedin style={{marginRight: "0.5rem"}}/>
+                            <a href="https://www.linkedin.com/in/klausfreire/">linkedin.com/klausfreire</a>
+                        </Col>
+                        <Col>
+                            <FaGithub style={{marginRight: "0.5rem"}}/>
+                            <a href="https://github.com/zenkairan">github.com/zenkairan</a>
+                        </Col>
                     </Container>
                     <div>
                     </div>
